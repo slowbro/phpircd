1f4a30aa831f56e76ef3ba02e50b0eb2393a8724
slowbro <slowbro@chatnets.net>
2010-07-09 12:30:34 +0800

    movin some shit around

--- core.class.php
+++ core.class.php
@@ -16,17 +16,7 @@ function __construct($config){
 	socket_bind($this->_socket,$this->address,$this->port);
 	socket_listen($this->_socket);
 	socket_set_nonblock($this->_socket);
-	echo "listening for new clients\n";
-	while(true){
-		if(($new = @socket_accept($this->_socket)) !== FALSE){
-			echo "new client\n";
-			$this->_clients[] = $new;
-			var_dump($this->_clients);
-		}
-	usleep(700);
-	}
 }
 
 }
-
 ?>
